eff5bd080fa33303e405b8bb5f144609a8fbc092
Sonia Becz <sonia.becz@onet.pl>
2023-06-01 10:25:26 +0200

    Initial commit

---
+++ src/main/java/com/example/itconferencerest/ItConferenceRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.itconferencerest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItConferenceRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItConferenceRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/itconferencerest/ItConferenceRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.itconferencerest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItConferenceRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
